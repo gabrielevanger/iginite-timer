8d18959224dff80dcfa9c90dc35023178be3c5b7
gabrielevanger <129545790+gabrielevanger@users.noreply.github.com>
2023-05-02 17:56:18 -0400

    Estilização do Ignite Time
    Estilização e correção de bugs
    
    Signed-off-by: gabrielevanger <129545790+gabrielevanger@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from 'styled-components'
+import { Button } from "./components/Button";
 
-import { Button } from "./components/Button"
-import { defaltTheme } from './components/styles/themes/default'
+import { defaultTheme } from './components/styles/themes/default'
+import { GlobalStyle } from './components/styles/global'
+
 export function App() {
 
   return (
-   <ThemeProvider theme={defaltTheme}>
+   <ThemeProvider theme={defaultTheme}>
     <Button variant="primary"/>
     <Button variant="secondary"/>
     <Button variant="success"/>
     <Button variant="danger"/>
     <Button />
+
+    <GlobalStyle />
    </ThemeProvider>
   )
 }
