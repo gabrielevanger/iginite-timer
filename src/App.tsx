0a5f8c546c9192e06990ee475005149e187463be
gabrielevanger <129545790+gabrielevanger@users.noreply.github.com>
2023-05-05 17:40:37 -0400

    Páginas e rotas
    Css projeto, e funções.
    
    Signed-off-by: gabrielevanger <129545790+gabrielevanger@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from "./components/Button";
-
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './components/styles/themes/default'
 import { GlobalStyle } from './components/styles/global'
+import { Router } from './components/Router'
 
 export function App() {
 
   return (
    <ThemeProvider theme={defaultTheme}>
-    <Button variant="primary"/>
-    <Button variant="secondary"/>
-    <Button variant="success"/>
-    <Button variant="danger"/>
-    <Button />
+   <BrowserRouter>
+     <Router />
 
+    </BrowserRouter>
     <GlobalStyle />
    </ThemeProvider>
   )
